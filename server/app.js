c13dbccb84e61a4d40d263e301d868cff8773141
WiktorKS <wiktorks1994@gmail.com>
2020-09-28 08:04:48 +0200

    innit

--- server/app.js
+++ server/app.js
@@ -4,12 +4,25 @@ const routes = require("./routes");
 const cors = require("cors");
 const passport = require("passport");
 const express = require("express");
-const app = express();
+const socketio = require("socket.io");
+const http = require("http");
+
 const { JWTStrategy } = require("./jwtStrategy");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+  formatMessage,
+} = require("./utils").userService;
 
 conn.connect((e) => {
   if (e) console.log(e);
 });
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(cors());
 
@@ -39,6 +52,56 @@ app.get(
   }
 );
 
-app.listen("3001", () => {
+io.on("connection", (socket) => {
+  socket.on("join", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // przywitanie użytkownika
+    socket.emit("message", formatMessage("Chat bot", "Welcome to ChatCord!"));
+
+    // Oznajmianie gdy użytkownik dołączy
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage("Chat bot", `${user.username} has joined the chat`)
+      );
+
+    // wysyłanie info o userach
+    io.to(user.room).emit("groupData", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on("sendMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // Gdy klient się rozłącza
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage("Chat bot", `${user.username} has left the chat`)
+      );
+
+      // wysyłanie info o userach
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
+server.listen("3001", () => {
   console.log("App is running on port 3001");
 });
--- server/database/database.connection.js
+++ server/database/database.connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "volvoxc70",
+  password: "password",
   database: "db",
 });
 
